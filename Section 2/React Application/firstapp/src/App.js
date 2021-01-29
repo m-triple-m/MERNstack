ab5c1736594c52a7e29a145f0c08ceddb5ff61d3
Mohammad-Mubassir <triplem656@gmail.com>
2021-01-29 13:00:32 +0530

    Class 8

---
+++ Section 2/React Application/firstapp/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import { Home, Login } from './components/home';
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to React App</h1>
+      <Home />
+      <Login />
+    </div>
+  );
+}
+
+export default App;
---
+++ Section 2/React Application/firstapp/src/components/home.js
@@ -0,0 +1,20 @@
+export const Home = () => {
+    return (
+        <div>
+            <h1>My Home Component</h1>
+            <p>Home page text</p>
+        </div>
+    )
+}
+
+export const Login = () => {
+    return (
+        <div>
+            <h1>My Login Component</h1>
+            <p>Login Here</p>
+        </div>
+    )
+}
+
+//for exporting single component
+// export default Home;
